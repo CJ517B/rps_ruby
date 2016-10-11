8513b39da73f03ab6e6c9f9f504e72efa69248dc
CJ517B <cj517b@users.noreply.github.com>
2016-10-11 10:46:47 -0500

    Remove test

--- rps.rb
+++ rps.rb
@@ -1,1 +1,22 @@
 # Rock, Paper, Scissors
+r = "rock"
+p = "paper"
+s = "scissors"
+
+puts "Welcome to Rock, Paper, Scissors!"
+puts "Your move: rock, paper, or scissors?"
+
+user_move=gets.chomp
+
+a =[r, p, s]
+
+computer_move = a[rand(a.length)]
+
+puts "My move is " + computer_move + "."
+
+if user_move == computer_move
+  puts "It's a tie!"
+elsif (user_move.downcase == r && computer_move == p) || (user_move.downcase == p && computer_move == s) || (user_move.downcase == s && computer_move == r)
+  puts "You lose :("
+else puts "You win! "
+end
