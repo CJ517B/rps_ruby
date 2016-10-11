75203a8310fba80d65472e2856fdadd0febfc932
CJ517B <cj517b@users.noreply.github.com>
2016-10-11 11:17:49 -0500

    version 3 work-in-progress

--- rps.rb
+++ rps.rb
@@ -2,23 +2,26 @@
 r = "rock"
 p = "paper"
 s = "scissors"
+a =[r, p, s]
 
 puts "Welcome to Rock, Paper, Scissors!"
 puts "Your move: rock, paper, or scissors?"
 
 user_move=gets.chomp
 
-a =[r, p, s]
-
-if a.include?(user_move.downcase)
-  computer_move = a[rand(a.length)]
-  puts "My move is " + computer_move + "."
-
-  if user_move == computer_move
-    puts "It's a tie!"
-  elsif (user_move.downcase == r && computer_move == p) || (user_move.downcase == p && computer_move == s) || (user_move.downcase == s && computer_move == r)
-    puts "You lose :("
-  else puts "You win! "
+while user_move.downcase != "quit"
+  if a.include?(user_move.downcase)
+    computer_move = a.sample
+    puts "My move is #{computer_move}."
+    if user_move == computer_move
+      puts "It's a tie!"
+    elsif (user_move.downcase == r && computer_move == p) || (user_move.downcase == p && computer_move == s) || (user_move.downcase == s && computer_move == r)
+      puts "You lose :("
+    else puts "You win! "
+    end
+  else puts "Nice try."
   end
-else puts "That doesn't seem to be a valid move."
+  puts "Let's start again."
+  user_move=gets.chomp
 end
+puts "Record:"
