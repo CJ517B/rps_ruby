447d6f1db01d2279c845833a92c4285470ecb214
CJ517B <cj517b@users.noreply.github.com>
2016-10-11 10:53:10 -0500

    version 2

--- rps.rb
+++ rps.rb
@@ -10,13 +10,15 @@ user_move=gets.chomp
 
 a =[r, p, s]
 
-computer_move = a[rand(a.length)]
+if a.include?(user_move.downcase)
+  computer_move = a[rand(a.length)]
+  puts "My move is " + computer_move + "."
 
-puts "My move is " + computer_move + "."
-
-if user_move == computer_move
-  puts "It's a tie!"
-elsif (user_move.downcase == r && computer_move == p) || (user_move.downcase == p && computer_move == s) || (user_move.downcase == s && computer_move == r)
-  puts "You lose :("
-else puts "You win! "
+  if user_move == computer_move
+    puts "It's a tie!"
+  elsif (user_move.downcase == r && computer_move == p) || (user_move.downcase == p && computer_move == s) || (user_move.downcase == s && computer_move == r)
+    puts "You lose :("
+  else puts "You win! "
+  end
+else puts "That doesn't seem to be a valid move."
 end
