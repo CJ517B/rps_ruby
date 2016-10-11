44a8ad1060c32c1ecb36999fa78e23c54df1cfe6
CJ517B <cj517b@users.noreply.github.com>
2016-10-11 11:34:35 -0500

    version 3

--- rps.rb
+++ rps.rb
@@ -7,6 +7,10 @@ a =[r, p, s]
 puts "Welcome to Rock, Paper, Scissors!"
 puts "Your move: rock, paper, or scissors?"
 
+ties = 0
+loss = 0
+wins = 0
+
 user_move=gets.chomp
 
 while user_move.downcase != "quit"
@@ -15,13 +19,17 @@ while user_move.downcase != "quit"
     puts "My move is #{computer_move}."
     if user_move == computer_move
       puts "It's a tie!"
+      ties = ties + 1
     elsif (user_move.downcase == r && computer_move == p) || (user_move.downcase == p && computer_move == s) || (user_move.downcase == s && computer_move == r)
       puts "You lose :("
-    else puts "You win! "
+      loss =  loss + 1
+    else
+      puts "You win! "
+      wins =  wins + 1
     end
   else puts "Nice try."
   end
   puts "Let's start again."
   user_move=gets.chomp
 end
-puts "Record:"
+puts "Record: wins: #{wins}; loss: #{loss}; ties: #{ties}"
